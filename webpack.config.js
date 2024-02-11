2f6d1cf669f33d7c543261940a7bc90140468496
nexus4812 <powerbatteryboy@gmail.com>
2024-02-11 16:15:31 +0900

    add serve command

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,12 @@ module.exports = {
         path: path.resolve(__dirname, 'public'),
         filename: 'main.js',
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
 };
 
